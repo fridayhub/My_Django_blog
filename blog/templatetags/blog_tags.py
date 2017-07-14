0c39d15f2731b1077bfc56d8032b939db6772ab2
liujinghang <hanglinux@163.com>
2017-07-14 21:55:31 +0800

    添加自定义模板,获取标签

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
+
+from django import template
+from ..models import Post, Category
+
+register = template.Library()
+
+#最新文章模板标签
+@register.simple_tag
+def get_recent_posts(num = 5): #默认返回前五条文章 按时间降序排列
+    return Post.objects.all().order_by('-create_time')[:num]
+
+#归档模板标签
+@register.simple_tag
+def archives():
+    #dates方法返回一个列表 每个元素为文章的创建时间,是Python的date对象,精确到月份,降序排列
+    return Post.objects.dates('create_time', 'month', order='DESC')
+
+#分类模板标签
+@register.simple_tag
+def get_categories():
+    return Category.objects.all()
